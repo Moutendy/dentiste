aaab578e8157a1e18d632ffb1ca6c883d2995041
Christopher Moutendy <c.moutendy@isicod.net>
2024-03-03 23:13:29 +0100

    connecter le back end avec le front et mettre les filter cros origine dans la partie back end

--- src/main/java/api/dentiste/apidentiste/controller/ConsultationController.java
+++ src/main/java/api/dentiste/apidentiste/controller/ConsultationController.java
@@ -26,7 +26,6 @@ import api.dentiste.apidentiste.util.inter.ConsultationOperation;
 @RestController
 @RequestMapping("api/consultation/")
 public class ConsultationController {
-	
 	protected ConsultationOperation consultationOperation;
 	protected ConsultationI consultationService;
 	
@@ -34,7 +33,6 @@ public class ConsultationController {
 		this.consultationOperation = consultationOperation;
 		this.consultationService = consultationService;
 	}
-	
 	   @PostMapping(value="addconsultation" ,consumes = "application/json")
 	   ResponseEntity<String> addConsultation(@RequestBody ConsutationAndUser consultation) {
 		   try {
@@ -44,7 +42,6 @@ public class ConsultationController {
 	            return new ResponseEntity<>("Failed to add consultation: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 	        }
 	    }
-	   
 	   @GetMapping(value="searchconsultationbydate/{date}", consumes = MediaType.APPLICATION_JSON_VALUE)
 	   ResponseEntity<List<ConsultationDto>> searchConsultationByDate(@PathVariable("date") String date) throws ParseException {
 		   SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
@@ -55,8 +52,6 @@ public class ConsultationController {
 	            return new ResponseEntity<>(consultationService.searchConsultationByDate(dateFrDate), HttpStatus.INTERNAL_SERVER_ERROR);
 	        }
 	   }
-	   
-	   
 	   @GetMapping(value="searchconsultationbyuser/{userName}", consumes = MediaType.APPLICATION_JSON_VALUE)
 	   ResponseEntity<List<ConsultationDto>> searchConsultationByUser(@PathVariable("userName")String userName) {
            try {
@@ -65,7 +60,6 @@ public class ConsultationController {
 	            return new ResponseEntity<>(consultationService.searchConsultationByUser(userName), HttpStatus.INTERNAL_SERVER_ERROR);
 	        }
 	   }
-	   
 	   @DeleteMapping(value="deleteconsultation/{id}" ,consumes = "application/json")
 	   ResponseEntity<String> deleteConsultation(@PathVariable("id") Long id) {
 		   try {
@@ -75,8 +69,6 @@ public class ConsultationController {
 	            return new ResponseEntity<>("Failed to add consultation: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 	        }
 	    }
-	   
-	   
 	   @GetMapping(value="getlistconsultation" ,produces = MediaType.APPLICATION_JSON_VALUE)
 	   ResponseEntity<List<ConsultationDto>> getListConsultation() {
 		   try {
@@ -85,8 +77,6 @@ public class ConsultationController {
 	            return new ResponseEntity<>(consultationService.getListConsultation(), HttpStatus.INTERNAL_SERVER_ERROR);
 	        }
 	    }
-	   
-	   
 	   @PutMapping(value="updateconsultation" ,consumes = MediaType.APPLICATION_JSON_VALUE)
 	   @Transactional
 	   void updateConsultation(@RequestBody ConsultationDto consultation) {
